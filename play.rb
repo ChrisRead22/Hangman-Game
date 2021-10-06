21cee32b93a60669547849885d55c394230773d7
Christopher Read <Christopherread22@outlook.com>
2021-10-05 21:01:50 -0400

    Game start

--- play.rb
+++ play.rb
@@ -8,10 +8,10 @@ class Hangman
     def words
         [
             ["cricket", "A game played by gentlemen"],
-            ["cricket", "A game played by gentlemen"],
-            ["cricket", "A game played by gentlemen"],
-            ["cricket", "A game played by gentlemen"],
-            ["cricket", "A game played by gentlemen"],
+            ["jogging", "We are not walking..."],
+            ["celebrate", "Remembering special moments"],
+            ["continent", "There are 7 of these"],
+            ["exotic", "Not from around here..."],
         ]
     end
 
